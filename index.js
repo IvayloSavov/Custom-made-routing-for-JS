4cbe6a68ddde9437ade5f7a8a08196e39fd204f1
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-10-23 13:52:27 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+// Factory function
+function Sammy(selector, initFn) {
+
+    const pathCollection = [];
+    let currentPath;
+
+    // Observer pattern
+    const core = {
+        get(path, fn) {
+            pathCollection.push({ path, fn });
+        },
+        redirect(path) {
+            currentPath = path;
+            this.render();
+        },
+        render() {
+            const pathObj = pathCollection.find(i => i.path === currentPath);
+            if (!pathObj) {
+                console.error(`body 404 Not found get ${currentPath}`);
+                return;
+            }
+            pathObj.fn();
+        }
+    }
+
+    const app = {
+        run(path) {
+            initFn.call();
+            core.redirect(path);
+        }
+    };
+
+
+
+    return app;
+}
+
+const app = Sammy("#main", function (params) {
+
+})
+app.run('/')
+
